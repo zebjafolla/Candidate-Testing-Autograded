78f060a101e0925704bb683e2b926bc0fec5536e
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-06-25 09:28:12 -0400

    Task 2 for candidate testing - tested with node  index.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,40 +11,56 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
-  candidateName = input.question("Please enter your name...");
+  candidateName = input.question("Please enter your name: ");
+  console.log("\n")
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
-
-candidateAnswer = input.question(question);
+for (let i = 0; i < questions.length; i ++)
+  {
+    let tempArr = []; 
+    tempArr.push(input.question(questions[i])); 
+    candidateAnswers[i] = tempArr;
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === "Sally Ride")
-  {
-    console.log("You are correct!");
-  }
-else
-{
-  console.log("You are incorrect. Please check for space, spelling and please do capitalize the first letters of each word.")
-}
-
-
+  for(let i = 0; i < candidateAnswers.length; i++)
+    {
+      if (candidateAnswers[i] != correctAnswers[i])
+        {
+          console.log(`Your answer was ${candidateAnswers[i]}, however the correct answer was ${correctAnswers[i]}. You were wrong.`);
+        }
+      else
+      {
+        console.log(`You were correct. Your answer was ${candidateAnswers[i]} and the correct answer was ${correctAnswers[i]}. Great job!`);
+      }
+    }
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
